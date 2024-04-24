f8abe35b67c35277760eb81edc748ea7bd1e0933
Nicolas Camenisch <garnele007@gmail.com>
2024-04-24 17:12:50 -0400

    Update detector ports / ip address in st.cmd

--- iocBoot/iocPSDPortDriver/st.cmd
+++ iocBoot/iocPSDPortDriver/st.cmd
@@ -9,7 +9,8 @@ dbLoadDatabase "dbd/psdPortDriver.dbd"
 psdPortDriver_registerRecordDeviceDriver pdbbase
 
 ## Setup PSD
-psdPortDriverConfigure("he3PSD", "127.0.0.1", "8765", "7654")
+# psdPortDriverConfigure("he3PSD", "127.0.0.1", "8765", "7654")
+# psdPortDriverConfigure("he3PSD", "192.168.0.17", "23", "4660")
 dbLoadRecords("db/psdPortDriver.template","P=he3PSD:,R=det1:,PORT=he3PSD,ADDR=0,TIMEOUT=1,NBINS=4")
 
 ## List available database records
